57273e0fd27e0c5b5f21ff05b8e6c0b2008b8c5c
Santiago Cano Restrepo <santiagocano6@hotmail.com>
2017-05-07 22:19:12 -0500

    Cotizacion

---
+++ Concesoft/Controllers/CotizacionController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Concesoft.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Concesoft.Controllers
+{
+    [Authorize]
+    public class CotizacionController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Cotizacion
+        public async Task<ActionResult> Index()
+        {
+            var CotizacionModels = db.CotizacionModels
+                .Include(f => f.ClienteModels)
+                .Include(x => x.ApplicationUser);            
+
+            return View(await CotizacionModels.ToListAsync());
+        }
+
+        // GET: Cotizacion/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CotizacionModels CotizacionModels = await db.CotizacionModels.FindAsync(id);
+            if (CotizacionModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(CotizacionModels);
+        }
+
+        // GET: Cotizacion/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClienteId = new SelectList(db.ClienteModels, "ClienteId", "Nombre");
+            ViewBag.UsuarioId = new SelectList(db.Users, "Id", "Nombre");
+
+            var articulos = db.RepuestoAccesorioModels;
+            var vehiculos = db.VehiculoModels;
+            ViewData["articulos"] = articulos;
+            ViewData["vehiculos"] = vehiculos;
+
+            return View();
+        }
+
+        // POST: Cotizacion/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,UsuarioId,ClienteId,ValorTotal")] CotizacionModels CotizacionModels)
+        {
+            CotizacionModels.UsuarioId = User.Identity.GetUserId();
+            CotizacionModels.ValorTotal = 0;
+
+            db.CotizacionModels.Add(CotizacionModels);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Edit/" + CotizacionModels.Id);
+        }
+
+        // GET: Cotizacion/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CotizacionModels CotizacionModels = await db.CotizacionModels.FindAsync(id);
+            if (CotizacionModels == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClienteId = new SelectList(db.ClienteModels, "ClienteId", "Nombre", CotizacionModels.ClienteId);
+            return View(CotizacionModels);
+        }
+
+        // POST: Cotizacion/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,UsuarioId,ClienteId,ValorTotal")] CotizacionModels CotizacionModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(CotizacionModels).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClienteId = new SelectList(db.ClienteModels, "ClienteId", "Nombre", CotizacionModels.ClienteId);
+            return View(CotizacionModels);
+        }
+
+        // GET: Cotizacion/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CotizacionModels CotizacionModels = await db.CotizacionModels.FindAsync(id);
+            if (CotizacionModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(CotizacionModels);
+        }
+
+        // POST: Cotizacion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            CotizacionModels CotizacionModels = await db.CotizacionModels.FindAsync(id);
+            db.CotizacionModels.Remove(CotizacionModels);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Concesoft/Controllers/DetalleCotizacionController .cs
@@ -0,0 +1,146 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using Concesoft.Models;
+using System.Collections;
+using System.Linq;
+
+namespace Concesoft.Controllers
+{
+    public class DetalleCotizacionController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        
+        public IEnumerable _ArticulosCotizacion(int CotizacionId)
+        {
+            return db.DetalleCotizacionModels
+                .Where(x => x.CotizacionId == CotizacionId)
+                .Include(d => d.RepuestoAccesorioModels)
+                .Include(x => x.VehiculoModels);
+        }
+
+        // GET: DetalleCotizacion
+        public async Task<ActionResult> Index()
+        {
+            var detalleCotizacionModels = db.DetalleCotizacionModels.Include(d => d.RepuestoAccesorioModels).Include(d => d.VehiculoModels);
+            return View(await detalleCotizacionModels.ToListAsync());
+        }
+
+        // GET: DetalleCotizacion/Details/5
+        public async Task<ActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleCotizacionModels detalleCotizacionModels = await db.DetalleCotizacionModels.FindAsync(id);
+            if (detalleCotizacionModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleCotizacionModels);
+        }
+
+        // GET: DetalleCotizacion/Create
+        public ActionResult Create(int CotizacionId)
+        {
+            ViewBag.CotizacionId = CotizacionId;
+            ViewBag.RepuestoAccesorioId = new SelectList(db.RepuestoAccesorioModels, "RepuestoAccesorioId", "NombreArticulo");
+            ViewBag.VehiculoId = new SelectList(db.VehiculoModels, "VehiculoId", "NombreVehiculo");
+            return View();
+        }
+
+        // POST: DetalleCotizacion/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,CotizacionId,VehiculoId,RepuestoAccesorioId,Cantidad,ValorTotal")] DetalleCotizacionModels detalleCotizacionModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DetalleCotizacionModels.Add(detalleCotizacionModels);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RepuestoAccesorioId = new SelectList(db.RepuestoAccesorioModels, "RepuestoAccesorioId", "NombreArticulo", detalleCotizacionModels.RepuestoAccesorioId);
+            ViewBag.VehiculoId = new SelectList(db.VehiculoModels, "VehiculoId", "NombreVehiculo", detalleCotizacionModels.VehiculoId);
+            return RedirectToAction("Edit/" + detalleCotizacionModels.CotizacionId, "Cotizacion");
+        }
+
+        // GET: DetalleCotizacion/Edit/5
+        public async Task<ActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleCotizacionModels detalleCotizacionModels = await db.DetalleCotizacionModels.FindAsync(id);
+            ViewBag.CotizacionId = detalleCotizacionModels.CotizacionId;
+            if (detalleCotizacionModels == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RepuestoAccesorioId = new SelectList(db.RepuestoAccesorioModels, "RepuestoAccesorioId", "NombreArticulo", detalleCotizacionModels.RepuestoAccesorioId);
+            ViewBag.VehiculoId = new SelectList(db.VehiculoModels, "VehiculoId", "NombreVehiculo", detalleCotizacionModels.VehiculoId);
+            return View(detalleCotizacionModels);
+        }
+
+        // POST: DetalleCotizacion/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,CotizacionId,VehiculoId,RepuestoAccesorioId,Cantidad,ValorTotal")] DetalleCotizacionModels detalleCotizacionModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(detalleCotizacionModels).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RepuestoAccesorioId = new SelectList(db.RepuestoAccesorioModels, "RepuestoAccesorioId", "NombreArticulo", detalleCotizacionModels.RepuestoAccesorioId);
+            ViewBag.VehiculoId = new SelectList(db.VehiculoModels, "VehiculoId", "NombreVehiculo", detalleCotizacionModels.VehiculoId);
+            return Content(Url.Action("Edit", "Cotizacion"));//RedirectToAction("Edit", "Cotizacion");
+        }
+
+        // GET: DetalleCotizacion/Delete/5
+        public async Task<ActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleCotizacionModels detalleCotizacionModels = await db.DetalleCotizacionModels.FindAsync(id);
+            ViewBag.CotizacionId = detalleCotizacionModels.CotizacionId;
+            if (detalleCotizacionModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleCotizacionModels);
+        }
+
+        // POST: DetalleCotizacion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(long id)
+        {
+            DetalleCotizacionModels detalleCotizacionModels = await db.DetalleCotizacionModels.FindAsync(id);
+            int CotizacionId = detalleCotizacionModels.CotizacionId;
+            db.DetalleCotizacionModels.Remove(detalleCotizacionModels);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Edit/" + detalleCotizacionModels.CotizacionId, "Cotizacion");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
